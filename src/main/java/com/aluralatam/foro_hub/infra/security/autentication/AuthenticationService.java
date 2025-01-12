16df685d83e29b70458f0c057125a008002f6157
Ramita20 <ramiro_larrea18@hotmail.com>
2025-01-12 20:41:52 -0300

    Configuración del proyecto, autenticación y autorización.

---
+++ src/main/java/com/aluralatam/foro_hub/infra/security/autentication/AuthenticationService.java
@@ -0,0 +1,22 @@
+package com.aluralatam.foro_hub.infra.security.autentication;
+
+import com.aluralatam.foro_hub.domain.usuario.UsuarioRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService implements UserDetailsService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public AuthenticationService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return usuarioRepository.findByNombre(username);
+    }
+}
